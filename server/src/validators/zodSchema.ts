cec2ba3664015d663f464422bc7d957b89bb717e
shubhamkv <shubh082002@gmail.com>
2025-05-17 20:24:08 +0530

    add activity schema

--- server/src/validators/zodSchema.ts
+++ server/src/validators/zodSchema.ts
@@ -10,3 +10,23 @@ export const loginSchema = z.object({
   username: z.string().email().trim(),
   password: z.string().min(6, "password must be at least 6 characters").trim(),
 });
+
+export const activitySchema = z.object({
+  url: z.string().url(),
+  domain: z.string(),
+  title: z.string(),
+  startTime: z.string().refine((val) => !isNaN(Date.parse(val)), {
+    message: "Invalid start time",
+  }),
+  endTime: z
+    .string()
+    .refine((val) => !isNaN(Date.parse(val)), { message: "Invalid end time" }),
+  isIdle: z.boolean(),
+  wasActiveTab: z.boolean(),
+  duration: z.number().int().nonnegative(),
+  sessionId: z.string().optional(),
+});
+
+export const paramsSchema = z.object({
+  sessionId: z.string().cuid(),
+});
