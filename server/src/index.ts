4a9e99623236a030472e62ae81cb649c9cbed958
shubhamkv <shubh082002@gmail.com>
2025-05-22 23:10:19 +0530

    add activities and session route

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import authRouter from "./routes/authRoutes";
+import activityRouter from "./routes/activityRoutes";
+import sessionRouter from "./routes/sessionRoutes";
 import { connectDB } from "./config/db";
 dotenv.config();
 
@@ -14,5 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
+app.use("/api/activities", activityRouter);
+app.use("/api/session", sessionRouter);
 
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
