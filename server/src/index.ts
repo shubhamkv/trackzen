e6026af980c2eb441b86cfd458aee922456089f2
shubhamkv <shubh082002@gmail.com>
2025-05-28 23:13:58 +0530

    add dashboard router

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import authRouter from "./routes/authRoutes";
 import activityRouter from "./routes/activityRoutes";
 import sessionRouter from "./routes/sessionRoutes";
+import dashboardRouter from "./routes/dashboardRoutes";
 import { connectDB } from "./config/db";
 dotenv.config();
 
@@ -18,5 +19,6 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/activities", activityRouter);
 app.use("/api/session", sessionRouter);
+app.use("/api/dashboard", dashboardRouter);
 
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
