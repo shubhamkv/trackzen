bd7a98fcd24d77bf1a12590a7dd9fe963c52b1fc
shubhamkv <shubh082002@gmail.com>
2025-05-29 19:42:39 +0530

    add weekly and monthly stats

--- server/src/controllers/dashboardController.ts
+++ server/src/controllers/dashboardController.ts
@@ -99,3 +99,183 @@ export const getDailyStats = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const getWeeklyStats = async (req: Request, res: Response) => {
+  if (!req.user) {
+    res.status(401).json({ msg: "User not authenticated" });
+    return;
+  }
+
+  const userId = req.user.id;
+
+  try {
+    const now = new Date();
+    const lastWeek = subHours(now, 168);
+
+    const activities = await prisma.websiteActivity.findMany({
+      where: {
+        userId,
+        startTime: {
+          gte: lastWeek,
+        },
+      },
+    });
+
+    if (!activities || activities.length === 0) {
+      res.status(404).json({
+        msg: "No activities found!!",
+      });
+      return;
+    }
+
+    const uniqueDomains = new Set(activities.map((a) => a.domain));
+    const totalWebsites = uniqueDomains.size;
+
+    const domainDuration: Record<string, number> = {};
+    for (const activity of activities) {
+      const domain = activity.domain;
+      domainDuration[domain] =
+        (domainDuration[domain] || 0) + activity.duration;
+    }
+
+    let topSite = "";
+    let topSiteTime = 0;
+
+    for (const [domain, time] of Object.entries(domainDuration)) {
+      if (time > topSiteTime) {
+        topSite = domain;
+        topSiteTime = time;
+      }
+    }
+
+    const sessions = await prisma.session.findMany({
+      where: {
+        userId,
+        startTime: {
+          gte: lastWeek,
+        },
+      },
+      orderBy: {
+        duration: "desc",
+      },
+    });
+
+    const totalDuration = sessions.reduce(
+      (sum, session) => sum + session.duration,
+      0
+    );
+    const averageTime =
+      sessions.length > 0 ? totalDuration / sessions.length : 0;
+
+    const longestSession = sessions.length > 0 ? sessions[0].duration : 0;
+
+    const response = {
+      activities,
+      topWebsite: {
+        domain: topSite,
+        duration: formatDuration(topSiteTime),
+      },
+      totalWebsites,
+      weeklyAverage: formatDuration(averageTime),
+      longestSession: formatDuration(longestSession),
+      totalDuration: formatDuration(totalDuration),
+    };
+
+    if (response) res.status(200).json(response);
+  } catch (error) {
+    console.error("Error fetching daily stats", error);
+    res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+};
+
+export const getMonthlyStats = async (req: Request, res: Response) => {
+  if (!req.user) {
+    res.status(401).json({ msg: "User not authenticated" });
+    return;
+  }
+
+  const userId = req.user.id;
+
+  try {
+    const now = new Date();
+    const lastMonth = subHours(now, 720);
+
+    const activities = await prisma.websiteActivity.findMany({
+      where: {
+        userId,
+        startTime: {
+          gte: lastMonth,
+        },
+      },
+    });
+
+    if (!activities || activities.length === 0) {
+      res.status(404).json({
+        msg: "No activities found!!",
+      });
+      return;
+    }
+
+    const uniqueDomains = new Set(activities.map((a) => a.domain));
+    const totalWebsites = uniqueDomains.size;
+
+    const domainDuration: Record<string, number> = {};
+    for (const activity of activities) {
+      const domain = activity.domain;
+      domainDuration[domain] =
+        (domainDuration[domain] || 0) + activity.duration;
+    }
+
+    let topSite = "";
+    let topSiteTime = 0;
+
+    for (const [domain, time] of Object.entries(domainDuration)) {
+      if (time > topSiteTime) {
+        topSite = domain;
+        topSiteTime = time;
+      }
+    }
+
+    const sessions = await prisma.session.findMany({
+      where: {
+        userId,
+        startTime: {
+          gte: lastMonth,
+        },
+      },
+      orderBy: {
+        duration: "desc",
+      },
+    });
+
+    const totalDuration = sessions.reduce(
+      (sum, session) => sum + session.duration,
+      0
+    );
+    const averageTime =
+      sessions.length > 0 ? totalDuration / sessions.length : 0;
+
+    const longestSession = sessions.length > 0 ? sessions[0].duration : 0;
+
+    const response = {
+      activities,
+      topWebsite: {
+        domain: topSite,
+        duration: formatDuration(topSiteTime),
+      },
+      totalWebsites,
+      monthlyAverage: formatDuration(averageTime),
+      longestSession: formatDuration(longestSession),
+      totalDuration: formatDuration(totalDuration),
+    };
+
+    if (response) res.status(200).json(response);
+  } catch (error) {
+    console.error("Error fetching daily stats", error);
+    res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+};
