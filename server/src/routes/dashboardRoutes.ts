0bae1a6965e36cb2732052c1299c75a757d1ce51
shubhamkv <shubh082002@gmail.com>
2025-05-31 22:16:58 +0530

    add post and get extension state route

--- server/src/routes/dashboardRoutes.ts
+++ server/src/routes/dashboardRoutes.ts
@@ -2,8 +2,10 @@ import express from "express";
 import { authMiddleware } from "../middleware/authMiddleware";
 import {
   getDailyStats,
+  getExtensionState,
   getMonthlyStats,
   getWeeklyStats,
+  postExtensionState,
 } from "../controllers/dashboardController";
 
 const router = express.Router();
@@ -13,5 +15,7 @@ router.use(authMiddleware);
 router.get("/daily-stats", getDailyStats);
 router.get("/weekly-stats", getWeeklyStats);
 router.get("/monthly-stats", getMonthlyStats);
+router.post("/extension-state", postExtensionState);
+router.get("/extension-state", getExtensionState);
 
 export default router;
